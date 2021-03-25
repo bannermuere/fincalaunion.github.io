fb8c5783e3aee5046173d5ad1bfefbc41a1cb26c
bannermuere <bannermuere@gmail.com>
2021-03-25 00:02:27 -0600

    v1.0

--- js/menu.js
+++ js/menu.js
@@ -6,3 +6,25 @@ document.getElementById("botonCerrar").addEventListener("click", cerrarMenu);
 function cerrarMenu(){
     document.getElementById("mostrarMenu").classList.toggle('mostrarLateral');
 }
+
+document.getElementById("botonArriba").addEventListener("click", scrollUp);
+
+function scrollUp(){
+    var currentScroll = document.documentElement.scrollTop;
+    if (currentScroll > 0){
+        window.scrollTo (0, currentScroll - (currentScroll / 0));
+    }
+}
+
+buttonUp = document.getElementById("botonArriba");
+
+window.onscroll = function(){
+
+    var scroll = document.documentElement.scrollTop;
+    if (scroll > 300){
+        buttonUp.style.transform = "scale(1)";
+    }else if(scroll < 300){
+        buttonUp.style.transform = "scale(0)";
+    }
+
+}
