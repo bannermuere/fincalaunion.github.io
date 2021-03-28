68b3a287da72e33c4db55c80ff44bee6cd99035c
bannermuere <bannermuere@gmail.com>
2021-03-28 04:17:33 -0600

    v1.2

--- js/menu.js
+++ js/menu.js
@@ -8,13 +8,20 @@ function cerrarMenu(){
 }
 
 window.onscroll = function(){
-
+    buttonUp = document.getElementById("botonArriba");
     var scroll = document.documentElement.scrollTop;
+
     if (scroll > 100){
         document.getElementById("iconoMenu").classList.add('iconoIzquierda');
         document.getElementById("iconoMenu").classList.remove('iconoDerecha');
     }else{
         document.getElementById("iconoMenu").classList.remove('iconoIzquierda');
         document.getElementById("iconoMenu").classList.add('iconoDerecha');
+    }
+
+    if (scroll > 300){
+        buttonUp.style.transform = "scale(1)";
+    }else if(scroll < 300){
+        buttonUp.style.transform = "scale(0)";
     }
 }
--- js/script.js
+++ js/script.js
@@ -7,16 +7,3 @@ function scrollUp(){
     }
 }
 
-buttonUp = document.getElementById("botonArriba");
-
-window.onscroll = function(){
-
-    var scroll = document.documentElement.scrollTop;
-
-    if (scroll > 300){
-        buttonUp.style.transform = "scale(1)";
-    }else if(scroll < 300){
-        buttonUp.style.transform = "scale(0)";
-    }
-
-}
