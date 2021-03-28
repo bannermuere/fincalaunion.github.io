c80777607d235a53e5637983888b3c8724b817b9
bannermuere <bannermuere@gmail.com>
2021-03-28 03:56:39 -0600

    v1.1.3

--- js/script.js
+++ js/script.js
@@ -2,7 +2,21 @@ document.getElementById("botonArriba").addEventListener("click", scrollUp);
 
 function scrollUp(){
     var currentScroll = document.documentElement.scrollTop;
-    if (currentScroll > 0){
+    if (currentScroll > 400){
         window.scrollTo (0, currentScroll - (currentScroll / 0));
     }
+}
+
+buttonUp = document.getElementById("botonArriba");
+
+window.onscroll = function(){
+
+    var scroll = document.documentElement.scrollTop;
+
+    if (scroll > 300){
+        buttonUp.style.transform = "scale(1)";
+    }else if(scroll < 300){
+        buttonUp.style.transform = "scale(0)";
+    }
+
 }
